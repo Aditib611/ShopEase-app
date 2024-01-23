494c18d192b2cb22e30ee384c8fd42c55e3d3606
Aditi Bhavsar <97657912+Aditib611@users.noreply.github.com>
2024-01-23 20:00:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 //configure env

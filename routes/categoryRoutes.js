fb3b79155c033bc69cbb3bd2bb3d147d87875a39
Aditi Bhavsar <97657912+Aditib611@users.noreply.github.com>
2024-01-23 19:13:05 +0530

    final codeecommerce mern stack app

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+import {
+  categoryController,
+  createCategoryController,
+  deleteCategoryController,
+  singleCategoryController,
+  updateCategoryController,
+} from "../controllers/CategoryController.js";
+//import { isAdmin } from "middlewaresauthMiddleware";
+
+const router = express.Router();
+
+//routes
+//create category
+router.post(
+  "/create-category",
+  requireSignIn,
+  isAdmin,
+  createCategoryController
+);
+
+//update category
+router.put(
+  "/update-category/:id",
+  requireSignIn,
+  isAdmin,
+  updateCategoryController
+);
+
+//get All Category
+router.get("/get-category", categoryController);
+
+//single category
+router.get("/single-category/:slug", singleCategoryController);
+
+//delete category
+router.delete(
+  "/delete-category/:id",
+  requireSignIn,
+  isAdmin,
+  requireSignIn,
+  deleteCategoryController
+);
+
+export default router;
